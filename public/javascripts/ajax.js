fa0a5ef4678bff667d7c02ffcb0fac0a9cce977d
D M Perendi <dmperendi1@sheffield.ac.uk>
2021-03-08 21:43:02 +0000

    ajax.js added with basic functionality

---
+++ public/javascripts/ajax.js
@@ -0,0 +1,12 @@
+function sendAjaxQuery(url, data){
+    $.ajax({
+        url: url,
+        type: "POST",
+        data: data,
+        contentType: 'application/json',
+        //Error function
+        error: err => console.log(err),
+        //Success function
+        success: response => $("#results").html(response)
+    });
+}
