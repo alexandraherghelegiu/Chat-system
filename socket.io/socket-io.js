d3d1fc6a064c219569032a6947379a720e027584
Alberto Pia <apia1@sheffield.ac.uk>
2021-03-09 17:17:09 +0300

    added join create and chat functions to socket-io file

--- socket.io/socket-io.js
+++ socket.io/socket-io.js
@@ -1,9 +1,29 @@
 
 exports.init = function(io) {
-  io.sockets.on('connection', function (socket) {
+
+  const chat = io
+      .of('/chat')
+      .on('connection', function (socket) {
     try {
-     // insert here your event
+      /**
+       * creates room
+       */
+      socket.on('create', function(room, userId, image){
+        socket.join(room);
+        chat.to(room).emit('created', room, userId, image);
+      });
+
+      socket.on('join', function(room, userId){
+        socket.join(room);
+        chat.to(room).emit('joined', room, userId);
+      });
+
+      socket.on('chat', function(room, userId, chatText){
+        io.to(room).emit('chat', room, userId, chatText);
+      });
+
     } catch (e) {
+      console.log(e);
     }
   });
 }
