0e441f46d798864b578d27dc9de40e9f83135cf3
D M Perendi <dmperendi1@sheffield.ac.uk>
2021-03-22 01:09:08 +0000

    Forms are now using Ajax, major refactoring in dashboard.js

--- Main Server/routes/dashboard.js
+++ Main Server/routes/dashboard.js
@@ -13,4 +13,12 @@ router.post('/', function(req, res, next) {
     res.render('dashboard', { name: name });
 });
 
+//POST the form data
+router.post("/processform", (req, res, next) => {
+   let body = req.body;
+   res.writeHead(200, {"Content-Type": "application/json"});
+   let data = JSON.stringify(body);
+   res.end(data);
+});
+
 module.exports = router;
