00dcbb1c5ed60090aeb201fe75b5000d5452c137
alexandraherghelegiu <aiherghelegiu1@sheffield.ac.uk>
2021-03-26 15:39:52 +0000

    deleted some unnecessary commented code

--- Main Server/app.js
+++ Main Server/app.js
@@ -34,7 +34,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- MongoDB Server/controllers/init.js
+++ MongoDB Server/controllers/init.js
@@ -7,16 +7,4 @@ exports.init= function() {
     images.remove({}, function(err) {
         console.log('collection removed')
     });
-
-    // const dob=new Date(1908, 12, 1).getFullYear();
-    // var character = new Character({
-    //     first_name: 'Mickey',
-    //     family_name: 'Mouse',
-    //     dob: dob
-    // });
-    // console.log('dob: '+character.dob);
-
-    // character.save(function (err, results) {
-    //     console.log(results._id);
-    // });
 }
