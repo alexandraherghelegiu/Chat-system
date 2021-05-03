5b0ff9b552e15693000d24d9efe12d7b59879bce
D M Perendi <dmperendi1@sheffield.ac.uk>
2021-05-03 12:05:49 +0100

    Added drawing rectangles functionality

--- Main Server/public/javascripts/annotations.js
+++ Main Server/public/javascripts/annotations.js
@@ -1,3 +1,7 @@
+var isDrawing = false;
+var startX;
+var startY;
+
 
 function annotationCanvasInit(){
     //Load current canvas
@@ -34,11 +38,92 @@ function annotationCanvasInit(){
                 }
             }, 10);
         });
-    })
-    //Add listeners
+
+        //Add listeners
+        addListeners(img, annotationCanvas, annotationCtx);
+    });
+
     //Pick different colours for different annotations
 }
 window.annotationCanvasInit = annotationCanvasInit;
+
+
+/**
+ * Adds the required listeners to the canvas
+ */
+function addListeners(image, canvas, ctx){
+    /**
+     * Handles the mousedown event
+     * @param e the event
+     */
+    function handleMouseDown(e) {
+        //Get mouse positions
+        let mousePos = getMousePos(canvas, e);
+        let mouseX = mousePos.x;
+        let mouseY = mousePos.y;
+
+        // If drawing
+        if (isDrawing) {
+            isDrawing = false;
+            ctx.beginPath();
+            ctx.rect(startX, startY, mouseX - startX, mouseY - startY);
+            ctx.stroke();
+            canvas.style.cursor = "default";
+            //TRIGGERS KG SEARCHBOX'S APPEARANCE
+        }
+        // If not drawing
+        else {
+            isDrawing = true;
+            startX = mousePos.x;
+            startY = mousePos.y;
+            canvas.style.cursor = "crosshair";
+        }
+    }
+
+    /**
+     * Handles the mousemove event
+     * @param e the event
+     */
+    function handleMouseMove(e) {
+        //Get mouse positions
+        let mousePos = getMousePos(canvas, e);
+        let mouseX = mousePos.x;
+        let mouseY = mousePos.y;
+
+        //Drawing rectangle
+        if (isDrawing) {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            drawImageScaled(image, canvas, ctx);
+            ctx.beginPath();
+            ctx.rect(startX, startY, mouseX - startX, mouseY - startY);
+            ctx.stroke();
+        }
+    }
+
+    //Adds listeners
+    $("#annotationCanvas").mousedown(function (e) {
+        handleMouseDown(e);
+    });
+
+    $("#annotationCanvas").mousemove(function (e) {
+        handleMouseMove(e);
+    });
+}
+
+
+/**
+ * Gets the real mouse position on a canvas
+ * @param canvas the canvas element
+ * @param evt the event
+ * @returns {{x: number, y: number}}
+ */
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
 
 function saveAnnotation(){
     //Save canvas as new canvas for the room
